fb567374ebfcc34f3f9edca4b7499be3fa5687b8
Rachel Isaac <rachelisaac995@gmail.com>
2023-02-28 13:46:02 +0400

    Add cart feature with 5 reducers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
+import { useSelector, useDispatch } from 'react-redux';
+import { calculateTotals } from './features/cart/cartSlice';
+import Modal from './components/Modal';
+import { useEffect } from 'react';
 function App() {
+  const { cartItems } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
   return (
-    <div className="App">
-      <h2>Redux Toolkit</h2>
-    </div>
+    <main>
+      <Modal />
+      <Navbar />
+      <CartContainer />
+    </main>
   );
 }
 
